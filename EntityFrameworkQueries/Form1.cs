30f49cd8b6fd0efcd9f7e9e7712b46e4743362f0
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-22 16:39:31 -0800

    example to select specific columns

--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace EntityFrameworkQueries
 {
     public partial class Form1 : Form
@@ -31,9 +33,34 @@ namespace EntityFrameworkQueries
                                         .ToList();
 
             List<Vendor> vendorList2 = (from v in dbContext.Vendors
-                                       where v.VendorState == "CA"
-                                       orderby v.VendorName
-                                       select v).ToList();
+                                        where v.VendorState == "CA"
+                                        orderby v.VendorName
+                                        select v).ToList();
         }
+
+        private void btnSelectSpecificColumns_Click(object sender, EventArgs e)
+        {
+            ApContext dbContext = new();
+
+            List<VendorLocation> results = (from v in dbContext.Vendors
+                          select new VendorLocation
+                          {VendorName = v.VendorName, 
+                           VendorState = v.VendorState, 
+                           VendorCity = v.VendorCity
+                          }).ToList();
+            StringBuilder displayString = new StringBuilder();
+            foreach(VendorLocation Vendor in results )
+            {
+                displayString.AppendLine($"{Vendor.VendorName} is in {Vendor.VendorCity}, {Vendor.VendorState}");
+            }
+            MessageBox.Show( displayString.ToString() );
+        }
+    }
+
+    class VendorLocation
+    {
+        public string VendorName { get; set; } 
+        public string VendorState { get; set;}
+        public string VendorCity { get; set;}
     }
 }
